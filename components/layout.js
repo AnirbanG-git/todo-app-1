86a310a3f9a28897c36fb8489fc0cf5562bf5657
AnirbanG-git <anir.dr@gmail.com>
2025-01-25 19:09:25 +0530

    changes

---
+++ components/layout.js
@@ -0,0 +1,9 @@
+import styles from '../styles/layout.module.css'
+export default function Layout(props) {
+  return (
+    <div className={styles.layout}>
+      <h1 className={styles.title}>To Do</h1>
+      {props.children}
+    </div>
+  )
+}
